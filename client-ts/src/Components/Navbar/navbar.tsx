9d837b2560c6b91b1e6e232ee935335ec0f77cb5
Thanaphat Panmas <tanapath.tp@gmail.com>
2023-02-16 22:17:39 +0700

    Create Navbar &Add animation

---
+++ client-ts/src/Components/Navbar/navbar.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import NavbarHeader from "./Navbar-feature/Navbar-header/navbarHeader";
+import NavbarMenu from "./Navbar-feature/Navbar-Menu/navbarMenu";
+import "./navbar.css";
+
+function NavBar() {
+  const [isHidden, setIsHidden] = useState(false);
+  let lastScrollY = window.scrollY;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (lastScrollY < window.scrollY) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${isHidden ? "navbar--hidden" : ""}`}>
+      <NavbarHeader />
+      <NavbarMenu/>
+    </nav>
+  );
+}
+
+export default NavBar;
