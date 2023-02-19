87dfae2a3b317669491e0e8d2fb630b02fe3d78b
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-19 20:24:21 +0700

    Add menu Logout & function remove jwt

--- client-ts/src/Components/Navbar/Navbar-feature/Navbar-header/MenuLogin/menuLogin.tsx
+++ client-ts/src/Components/Navbar/Navbar-feature/Navbar-header/MenuLogin/menuLogin.tsx
@@ -1,12 +1,39 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./menuLogin.css";
 
 function MenuLogin() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("jwt");
+    setIsLoggedIn(jwt ? true : false);
+  }, []);
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("jwt");
+    setIsLoggedIn(false);
+    navigate("/");
+  };
+
   return (
-    <div className="menuLogin">
-      <a className="menulogin-link" href="/login">
-        LOGIN
-      </a>
-    </div>
+    <nav className="menuLogin">
+      {isLoggedIn ? (
+        <a className="menulogin-link" onClick={handleLogout}>
+          LOGOUT
+        </a>
+      ) : (
+        <a className="menulogin-link" onClick={handleLogin}>
+          LOGIN
+        </a>
+      )}
+    </nav>
   );
 }
 
--- client-ts/src/Config/routes.tsx
+++ client-ts/src/Config/routes.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import Home from "../Pages/Home/home";
 import Login from "../Pages/Login/login";
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <ToastContainer />
+    </div>
   );
 }
 
