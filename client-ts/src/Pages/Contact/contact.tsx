507d2ddb795b8315401e8c6816b496f603d3b36f
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-22 14:40:51 +0700

    Create contact page >Grid

--- client-ts/src/Pages/Contact/contact.tsx
+++ client-ts/src/Pages/Contact/contact.tsx
@@ -1,28 +1,24 @@
 import Footer from "../../Components/Footer/footer";
 import Navbar from "../../Components/Navbar/navbar";
-import MainContent from "../../Components/Main-Content/main-content";
-import SearchBar from "../../Components/Search-bar/search-bar";
-import FilterSelection from "../../Components/Filter-Selection/filter-selection";
+
 import "./contact.css"
 
 function Contact() {
-    const coverHome = require("../../Assets/cover_homePage/coverHome.jfif");
-    return (
-      <div className="home-container">
-        <div className="home-h-e-a-d"></div>
-        <img className="coverHome" src={coverHome} alt="cover" />
-        <div className="home-m-i-d-d-l-e">
-          <div className="home-main">
-            <div className="home-main-option">
-            </div>
-          </div>
+  const coverHome = require("../../Assets/cover_homePage/coverHome.jfif");
+  return (
+    <div className="home-container">
+      <div className="home-h-e-a-d"></div>
+      <img className="coverHome" src={coverHome} alt="cover" />
+      <div className="home-m-i-d-d-l-e">
+        <div className="home-main">
         </div>
-        <div className="home-f-o-o-t">
-          <Footer />
-        </div>
-        <Navbar />
       </div>
-    );
-  }
-  
-  export default Contact ;
+      <div className="home-f-o-o-t">
+        <Footer />
+      </div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Contact;
