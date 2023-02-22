d87e24601ec6a3cecb252a719044f71b1fdb6b98
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-22 23:10:46 +0700

    Update Page contact

---
+++ client-ts/src/Components/Main-Contact/main-contact.tsx
@@ -0,0 +1,103 @@
+import "./main-contact.css";
+
+function MainContact() {
+  return (
+    <div className="MainContact-container">
+      <header className="MainContact-topic">
+        <h1 className="MainContact-heading-topic">Contact Us</h1>
+      </header>
+      <div className="MainContact-contact">
+        <div className="MainContact-row">
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+        </div>
+        <div className="MainContact-row">
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+          <div className="MainContact-person">
+            <div className="MainContact-image">
+              <img src="" alt="" className="MainContact-cover-image" />
+            </div>
+            <div className="MainContact-information">
+              <div className="MainContact-name">
+                <h1 className="MainContact-text">Silmee Panan</h1>
+              </div>
+              <div className="MainContact-other">
+                <span>6510110116</span>
+              </div>
+            </div>
+            <div className="MainContact-button"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainContact;
