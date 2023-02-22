a443b0e0742b9404eb30707c6a2bc7d92df85334
ป่าปัญญา สิริวัฒนาวรกุล <papanya2546@gmail.com>
2023-02-22 21:18:26 +0700

    เพิ่มmeilisearch

---
+++ server-ts/config/plugins.ts
@@ -0,0 +1,11 @@
+module.exports = () => ({
+    //...
+    meilisearch: {
+      config: {
+        // Your meili host
+        host: "https://ms-6aa85f678e03-2164.sgp.meilisearch.io",
+        // Your master key or private key
+        apiKey: "1230e7fe39f7fa2dcc3125dfde8f9b8f53b38776",
+      }
+    }
+  })
