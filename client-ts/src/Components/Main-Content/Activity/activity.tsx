eb2c6a3d5a546443d33752912da958fdb5fbd058
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-24 01:30:59 +0700

    Use getAll

--- client-ts/src/Components/Main-Content/Activity/activity.tsx
+++ client-ts/src/Components/Main-Content/Activity/activity.tsx
@@ -1,26 +1,42 @@
+import { useEffect, useState } from "react";
+import { ActivityRepository } from "../../../Repositories/ActivityRepository";
+import ModelActivity from "../../../Repositories/Models/Activity";
+
 import "./activity.css";
+import "../main-content.css";
+
+const cover_activity = require("../../../Assets/cover_activity/activity-1.png");
 
 function Activity() {
-  const cover_activity = require("../../../Assets/cover_activity/activity-1.png");
+  const activityRepository = new ActivityRepository();
+  const [activities, setActivities] = useState<ModelActivity[] | null>(null);
+
+  useEffect(() => {
+    activityRepository.getAll().then((result) => setActivities(result));
+  }, [activityRepository]);
+
+  if (activities === null) {
+    return <div>ไม่มีข้อมูล</div>;
+  }
 
   return (
-    <div className="activity">
-      <div className="activity-image">
-        <img className="image" src={cover_activity} alt="" />
-      </div>
-      <div className="activity-text">
-        <div className="activity-title">
-          <h1 className="title">ZERO To Javascript</h1>
+    <>
+      {activities.map((activity: ModelActivity) => (
+        <div key={activity.id} className="activity">
+          <div className="activity-image">
+            <img className="image" src={cover_activity} alt="" />
+          </div>
+          <div className="activity-text">
+            <div className="activity-title">
+              <h1 className="title">{activity.title}</h1>
+            </div>
+            <div className="activity-description">
+              <span className="description">{activity.description}</span>
+            </div>
+          </div>
         </div>
-        <div className="activity-description">
-          <span className="description">
-            หลักสูตรใหม่ ก้าวสู่การเป็น javascript dev ในระดับ intermediate
-            เรียนตั้งแต่เรื่องแรกยันเรื่องสุดท้าย เทคนิคการสอนแบบใหม่
-            หลักสูตรใหม่ เนื้อหาแน่นกว่าเดิมม!!!
-          </span>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
 
