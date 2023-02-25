ed7fb09bcdb63ed902ee3d496da0fcf4e9edfe99
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-25 17:59:10 +0700

    useParams in activityDetailForm from >{activityId}

--- client-ts/src/Components/Activity-Detail-Form/activityDetailForm.tsx
+++ client-ts/src/Components/Activity-Detail-Form/activityDetailForm.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import ModelActivity from "../../Models/Activity";
+import Repo from "../../Repositories/index";
 
 function ActivityDetailForm() {
+  const { activityId } = useParams<{ activityId: string }>();
+  const [activity, setActivity] = useState<ModelActivity | null>(null);
+
+  const fetchData = async () => {
+    const res = await Repo.ActivityRepository.getActivityById(
+      Number(activityId)
+    );
+    if (res) {
+      setActivity(res);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [activityId]);
 
   return (
     <div>
-      <h1>Title</h1>
-      <p>Description</p>
+      <h1>{activity?.attributes.title}</h1>
+      <p>{activity?.attributes.description}</p>
     </div>
   );
 }
