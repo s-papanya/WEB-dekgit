8680b90515a2587e7fa9c450121c5f13b66ccebd
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-02-25 17:55:46 +0700

    Create ActivityDetailForm

---
+++ client-ts/src/Components/Activity-Detail-Form/activityDetailForm.tsx
@@ -0,0 +1,12 @@
+
+function ActivityDetailForm() {
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Description</p>
+    </div>
+  );
+}
+
+export default ActivityDetailForm;
--- client-ts/src/Pages/Activity-Detail/activityDetail.tsx
+++ client-ts/src/Pages/Activity-Detail/activityDetail.tsx
@@ -1,7 +1,24 @@
-import "./activityDetail.css"
+import Footer from "../../Components/Footer/footer";
+import Navbar from "../../Components/Navbar/Navbar/navbar";
+import ActivityDetailForm from "../../Components/Activity-Detail-Form/activityDetailForm";
 
-function ActivityDetail(){
-    return null;
+function ActivityDetail() {
+  const coverHome = require("../../Assets/cover_homePage/coverHome.jfif");
+  return (
+    <div className="home-container">
+      <div className="home-h-e-a-d"></div>
+      <img className="coverHome" src={coverHome} alt="cover" />
+      <div className="home-m-i-d-d-l-e">
+        <div className="home-main">
+          <ActivityDetailForm />
+        </div>
+      </div>
+      <div className="home-f-o-o-t">
+        <Footer />
+      </div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default ActivityDetail
+export default ActivityDetail;
