fa1593fc5b9dd1ad32cf549f070297f430ec4d81
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-03-01 23:42:05 +0700

    Add method createActivity

--- client-ts/src/Repositories/ActivityRepository.ts
+++ client-ts/src/Repositories/ActivityRepository.ts
@@ -1,10 +1,14 @@
 import { IRepository } from "./IRepository";
 import ModelActivity from "../Models/Activity";
+import { userData } from "../Config/provider";
+
+const user = userData()
 
 import config from "../Config/config";
 
 export class ActivityRepository implements IRepository<ModelActivity> {
   urlPrefix = config.remoteRepositoryUrlPrefix;
+  token = user.jwt
 
   async getActivity(): Promise<ModelActivity[] | null> {
     const res = await fetch(`${this.urlPrefix}/activities`);
@@ -14,6 +18,19 @@ export class ActivityRepository implements IRepository<ModelActivity> {
 
   async getActivityById(id: string | number): Promise<ModelActivity | null> {
     const res = await fetch(`${this.urlPrefix}/activities/${id}`);
+    const data = await res.json();
+    return data.data;
+  }
+
+  async createActivity(entity: Partial<ModelActivity>) {
+    const res = await fetch(`${this.urlPrefix}/activities`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(entity),
+    });
     const data = await res.json();
     return data.data;
   }
