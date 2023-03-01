739d2ad175ec75aa207e02248db807c21e0d5f55
ป่าปัญญา สิริวัฒนาวรกุล <papanya2546@gmail.com>
2023-03-02 01:53:01 +0700

    create Activity (not done)

--- client-ts/src/Components/Create-Activity/createActivity.tsx
+++ client-ts/src/Components/Create-Activity/createActivity.tsx
@@ -1,7 +1,51 @@
-import "./createActivity.css"
+import { useState } from 'react';
+import { Button, Input } from '@mui/material';
+import axios from 'axios';
 
-function CreateActivity() {
-  return <div>Only Admin</div>
+interface ICreate{
+    title : string
 }
 
-export default CreateActivity;
+function CreateActivity() {
+    const initialCreate: ICreate = { title:" " };
+    const [create, setCreate] = useState<ICreate>(initialCreate);
+
+    const handleChange = ({
+    target,
+    }: {
+    target: { name: string; value: string };
+    }): void => {
+    const { name, value } = target;
+    setCreate((currententity) => ({
+        ...currententity,
+        [name]: value,
+    }));
+    };
+
+    const onCreateActivity = async (): Promise<void> => {
+        try {
+          const url = `http://localhost:1337/api/auth/local/register`
+          const data = await axios.post(url,create)
+          console.log(data.data);
+        } catch (error: any) {
+          console.log(error);
+        }
+      };
+
+    return (
+        <div>
+            <Input
+                type="text"
+                name="title"
+                value={create.title}
+                onChange={handleChange}
+                placeholder="Name"
+            />
+            <Button  onClick={onCreateActivity}>
+                Create
+            </Button>
+        </div>
+    )
+}
+
+export default CreateActivity
