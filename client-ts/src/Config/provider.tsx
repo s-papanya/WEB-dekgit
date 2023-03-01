a6f75a0eee567cf5166b99bda65d7deb54537960
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-03-01 23:34:50 +0700

    Add storeRole in provider

--- client-ts/src/Config/provider.tsx
+++ client-ts/src/Config/provider.tsx
@@ -1,10 +1,51 @@
-export const storeUser = (data:any) => {
-    localStorage.setItem(
-        'user', 
-        JSON.stringify({
-            username: data.user.username,
-            admin: data.user.admin,
-            jwt: data.jwt,
-        })
-    )
-}
+import { useEffect } from "react";
+
+export const storeUser = (data: any) => {
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      username: data.user.username,
+      jwt: data.jwt,
+    })
+  );
+};
+
+export const userData = () => {
+  const stringfiedUser = localStorage.getItem("user") || '""';
+  if (stringfiedUser) {
+    return JSON.parse(stringfiedUser);
+  }
+  return false;
+};
+
+export const storeRole = (data: any) => {
+  localStorage.setItem(
+    "role",
+    JSON.stringify({
+      role: data.role.type,
+    })
+  );
+};
+
+export const roleData = () => {
+  const stringfiedUser = localStorage.getItem("role") || '""';
+  if (stringfiedUser) {
+    return JSON.parse(stringfiedUser);
+  }
+  return false;
+};
+
+type Props = {
+  children: JSX.Element;
+};
+
+export const ProtectRoute = ({ children }: Props) => {
+  const { jwt } = userData();
+  useEffect(() => {
+    if (!jwt) {
+      console.log("aaaaa");
+    }
+  }, [jwt]);
+
+  return children;
+};
