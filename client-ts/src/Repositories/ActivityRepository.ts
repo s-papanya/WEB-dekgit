bffa4b732cfee00adb292facf7eec95348cb9511
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-03-07 09:29:43 +0700

    Edit method CreateActivity

--- client-ts/src/Repositories/ActivityRepository.ts
+++ client-ts/src/Repositories/ActivityRepository.ts
@@ -1,13 +1,15 @@
 import { IRepository } from "./IRepository";
 import GetActivity from "../Models/getActivity";
-import PostActivity from "../Models/postActivity"
+import PostActivity from "../Models/postActivity";
 import config from "../Config/config";
 
 import { userData } from "../Config/provider";
 
 const user = userData();
 
-export class ActivityRepository implements IRepository<GetActivity | PostActivity> {
+export class ActivityRepository
+  implements IRepository<GetActivity | PostActivity>
+{
   urlPrefix = config.remoteRepositoryUrlPrefix;
 
   async getActivity(): Promise<GetActivity[] | null> {
@@ -22,22 +24,25 @@ export class ActivityRepository implements IRepository<GetActivity | PostActivit
     return data.data;
   }
 
-  async createActivity(entity: Partial<PostActivity>) {
-    const res = await fetch(`http://localhost:1337/api/activities`, {
+  async createActivity(data: PostActivity): Promise<PostActivity> {
+    const resp = await fetch(`http://localhost:1337/api/activities/`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ` + user.jwt,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(entity),
+      body: JSON.stringify(data),
     });
-    const data = await res.json();
-    return data;
+    const res = await resp.json();
+    return res.data;
   }
 
-  async updateActivity(id: string | number, data: PostActivity): Promise<PostActivity> {
-    const res = await fetch(`http://localhost:1337/api/activities/${id}`, {
+  async updateActivity(
+    id: string | number,
+    data: PostActivity
+  ): Promise<PostActivity> {
+    const resp = await fetch(`http://localhost:1337/api/activities/${id}`, {
       method: "PUT",
       headers: {
         Authorization: `Bearer ` + user.jwt,
@@ -46,7 +51,7 @@ export class ActivityRepository implements IRepository<GetActivity | PostActivit
       },
       body: JSON.stringify(data),
     });
-    const res_data = await res.json();
-    return res_data.data;
+    const res = await resp.json();
+    return res.data;
   }
 }
