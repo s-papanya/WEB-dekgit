de9ee3bc1214f87984c3fdd69a9a756cd099e9a9
ธนภัทร ปานมาส <tanapath.tp@gmail.com>
2023-03-07 16:20:52 +0700

    Add form create >grid

--- client-ts/src/Pages/Admin-CreateActivity/admin-createActivity.tsx
+++ client-ts/src/Pages/Admin-CreateActivity/admin-createActivity.tsx
@@ -2,6 +2,7 @@ import CreateActivity from "../../Components/Create-Activity/createActivity";
 import Footer from "../../Components/Footer/footer";
 import Navbar from "../../Components/Navbar/Navbar/navbar";
 
+import "./admin-createActivity.css";
 
 function AdminCreateActivity() {
   const coverHome = require("../../Assets/cover_homePage/coverHome.jfif");
@@ -12,7 +13,17 @@ function AdminCreateActivity() {
       <img className="coverHome" src={coverHome} alt="cover" />
       <div className="home-m-i-d-d-l-e">
         <div className="home-main">
-          <CreateActivity/>
+          <div className="activity-detail-from-container ">
+            <div className="activity-detail-from-redline"></div>
+            <header className="activity-detail-from-topic">
+              <h1 className="activity-detail-from-text">Create Activity</h1>
+            </header>
+            <div className="admin-createActivity-con">
+              <div className="admin-createActivity">
+                <CreateActivity />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="home-f-o-o-t">
